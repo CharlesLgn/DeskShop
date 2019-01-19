69cb50e6cc244af2f77b66056d02c5f04ff0d2c9
cyril <cyril@challouatte.com>
2019-01-19 11:10:23 +0100

    FXML

---
+++ src/main/java/com/front/controllers/DisplayArticleController.java
@@ -0,0 +1,31 @@
+package com.front.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class DisplayArticleController {
+
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private ImageView imgProduit;
+
+    @FXML
+    private Label lbNomProduit;
+
+    @FXML
+    private Label lbPrix;
+
+    @FXML
+    private Hyperlink linkAjouterPanier;
+
+    @FXML
+    void linkAjouterPanierClick(ActionEvent event) {
+
+    }
+}
