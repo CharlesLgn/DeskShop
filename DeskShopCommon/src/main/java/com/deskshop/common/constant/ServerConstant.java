5ecea1f424fc038d052577d41efb20e7ae99d06a
CharlesLgn <charles.ligony3@etu.univ-lorraine.fr>
2019-01-24 20:05:39 +0100

    jdkdjljé

--- DeskShopCommon/src/main/java/com/deskshop/common/constant/ServerConstant.java
+++ DeskShopCommon/src/main/java/com/deskshop/common/constant/ServerConstant.java
@@ -1,15 +1,11 @@
 package com.deskshop.common.constant;
 
-import com.deskshop.common.link.BankInterface;
 import com.deskshop.common.link.ClientInterface;
 import com.deskshop.common.link.ServerInterface;
-import com.deskshop.common.link.ShopInterface;
 
 public class ServerConstant {
     public static final int PORT  = 4000;
     public static final String IP = "localhost";
-    public static BankInterface BANK;
-    public static ShopInterface SHOP;
     public static ServerInterface SERVER;
     public static ClientInterface CLIENT;
 }
