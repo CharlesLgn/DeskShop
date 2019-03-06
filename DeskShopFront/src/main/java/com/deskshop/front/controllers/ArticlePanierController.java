265329e46b3f18de868c2beedd13514d6e51b1e1
Cyril Challouatte <you@example.com>
2019-03-06 16:13:52 +0100

    add front

--- DeskShopFront/src/main/java/com/deskshop/front/controllers/ArticlePanierController.java
+++ DeskShopFront/src/main/java/com/deskshop/front/controllers/ArticlePanierController.java
@@ -1,6 +1,7 @@
 package com.deskshop.front.controllers;
 
 import com.deskshop.common.metier.Article;
+import com.deskshop.front.util.ControllerUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -11,6 +12,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class ArticlePanierController implements Initializable {
@@ -43,11 +45,14 @@ public class ArticlePanierController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //this.imgImageArticle.setImage(new Image(article.getPicture()));
-        this.imgImageArticle.setImage(new Image("images/deskshop-logo.png"));
-        this.lbNomArticle.setText(this.article.getName());
-        this.lbPrixArticle.setText(this.article.getPrice()+"Ø");
-        this.lbDesc.setText(this.article.getDesc());
-        this.spinnerQuantite.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1,Integer.MAX_VALUE, qte,1));
+        try {
+            this.imgImageArticle.setImage(new Image(Paths.get(this.article.getPicture()).toUri().toURL().toExternalForm()));
+            this.lbNomArticle.setText(this.article.getName());
+            this.lbPrixArticle.setText(this.article.getPrice() + "Ø");
+            this.lbDesc.setText(this.article.getDesc());
+            this.spinnerQuantite.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, Integer.MAX_VALUE, qte, 1));
+        }catch (Exception ex){
+            ControllerUtils.loadAlert("Erreur lors de la récupération des informations sur l'article", ex.toString());
+        }
     }
 }
